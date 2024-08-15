5161e88966074ff8c14d11da8eec063e1afc99d0
desouki77 <mahmoud.desouki77@gmail.com>
2024-08-15 23:49:53 +0300

    update admin login and dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
-import AdminDashboard from './components/AdminDashboard';
+import AdminDashboard from './components/Admin/AdminDashboard';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Login} />
+        <Route exact path="/admin-login" component={Login} />
         <Route path="/admin-dashboard" component={AdminDashboard} />
       </Switch>
     </Router>
